466b46b5c3521dcf062714da77456422193ee831
Duharter <duharter@ufl.edu>
2019-09-27 13:05:37 -0400

    3/4 tests done

--- Bootcamp3/server/controllers/listings.server.controller.js
+++ Bootcamp3/server/controllers/listings.server.controller.js
@@ -57,6 +57,30 @@ exports.read = function(req, res) {
 exports.update = function(req, res) {
   var listing = req.listing;
 
+  listing.code = req.body.code;
+  listing.name = req.body.name;
+  listing.address = req.body.address;
+  if(req.results)
+  {
+    listing.coordinates = {
+      latitude: req.results.lat,
+      longitude: req.results.lng
+    };
+  }
+
+  listing.save(function(err)
+  {
+    if (err)
+    {
+      console.log(err);
+      res.status(400).send(err);
+    }
+    else
+    {
+      res.json(listing);
+    }
+  });
+
   /* Replace the listings's properties with the new properties found in req.body */
  
   /*save the coordinates (located in req.results if there is an address property) */
@@ -69,13 +93,33 @@ exports.update = function(req, res) {
 exports.delete = function(req, res) {
   var listing = req.listing;
 
-  /* Add your code to remove the listins */
+  listing.remove(function(err) {
+    if (err) 
+    {
+      console.log(err);
+      res.status(400).send(err);
+    } 
+    else
+    {
+      res.end();
+    }
+  });
 
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
   /* Add your code */
+  Listing.find().sort('code').exec(function(err, listings) {
+    if (err)
+    {
+      res.status(400).send(err);
+    } 
+    else 
+    {
+      res.json(listings);
+    }
+  });
 };
 
 /* 
