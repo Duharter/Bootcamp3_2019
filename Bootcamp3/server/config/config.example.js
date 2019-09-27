dfee2ddbc74133a43eb34706b9ec22b88f55ca43
Duharter <duharter@ufl.edu>
2019-09-27 11:34:38 -0400

    First commit

--- Bootcamp3/server/config/config.example.js
+++ Bootcamp3/server/config/config.example.js
@@ -4,10 +4,10 @@
 
 module.exports = {
   db: {
-    uri: ''//place the URI of your mongo database here.
+    uri: 'mongodb+srv://duharter:Julito90@@cluster0-kds1v.mongodb.net/test?retryWrites=true&w=majority',
   }, 
   openCage: {
-    key: '' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+    key: 'fbf377ec120f4dc5a8dbc78783370097' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
   },
   port: 8080
 };
---
+++ Bootcamp3/server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+      uri: 'mongodb+srv://duharter:Julito90@@cluster0-kds1v.mongodb.net/test?retryWrites=true&w=majority'
+    }, 
+    openCage: {
+      key: 'fbf377ec120f4dc5a8dbc78783370097' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+    },
+    port: 8080
+  };
